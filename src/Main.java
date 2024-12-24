6cbb0eb7527b81e8743dcc0b675a950238402013
Patricia82TI <patricia.melloti@gmail.com>
2024-12-24 04:48:22 -0300

    Exercicio de função vazia que imprime dez vezes Bom dia

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+
+//Faça um programa que imprima "BOM DIA" 10 vezes na tela usando uma função
+public class Main {
+
+    static void imprimeBomDia(){
+        System.out.println("Bom dia");
+    }
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+           imprimeBomDia();
+        }
+    }
+}
